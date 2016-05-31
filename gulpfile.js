216c8a4e09a0b8ebcf1861729380d72df60c9bb9
Fernando Farias <nandofarias@me.com>
2016-05-31 11:55:30 -0300

    Adding credentials from social

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
 var gulp = require('gulp');
+var nodemon = require('nodemon');
+var env = require('gulp-env');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('nodemon', function() {
+    nodemon({
+        script: 'server/index.js'
+        , ext: 'js html'
+        , env: { 'NODE_ENV': 'development' }
+    })
 });
+
+gulp.task('set-env', function () {
+    env({
+        vars: {
+            FACEBOOK_ID: '1609192872732615',
+            FACEBOOK_SECRET: '4301ae0ab11854cb567cdf3450b0ae90',
+            TWITTER_ID: 'BDyX62NVaztgoJ2IRJ7vRNMWC',
+            TWITTER_SECRET: 'TgrakP9qZ0GIIzbTRpOFOOVonM3DQDbNosypYqT3EtoiPYedpJ',
+            GOOGLE_ID: '218161342051-qprqo3ssbp07k2897cs8toifod6ujvnd.apps.googleusercontent.com',
+            GOOGLE_SECRET:'QSaer7Ghx6t9U2GiXXw4ay-W',
+            DOMAIN:'http://localhost:3000',
+            PORT: 3000
+        }
+    })
+});
+
+gulp.task('default', ['set-env', 'nodemon'])
