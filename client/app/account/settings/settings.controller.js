e3bf4de925f7391fc7674a9720dd14d111956bc5
Fernando Farias <nandofarias@me.com>
2016-06-14 13:49:05 -0300

    Changing password ok and header layout fixed

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -11,13 +11,24 @@
     function settingsController(Auth) {
         var vm = this;
         vm.title = 'settingsController';
+        vm.user = {};
 
         activate();
 
         ////////////////
 
         function activate() {
-            
+            vm.updatePassword = updatePassword;
+        }
+
+        function updatePassword() {
+            Auth.changePassword(vm.user.oldPassword, vm.user.newPassword)
+                .then(function () {
+                    vm.message = "Senha alterada com sucesso";
+                })
+                .catch(function (err) {
+                    vm.message = "Senha incorreta, tente novamente!";
+                })
         }
     }
 
